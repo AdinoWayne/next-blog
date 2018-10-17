18fdc57fcd6a167e925023911e0f5f6cbc9b9a14
Tran Quan Trung <gnurt10@gmail.com>
2018-10-17 08:39:44 +0700

    show and add post management

--- pages/manages/post-management/index.js
+++ pages/manages/post-management/index.js
@@ -1,11 +1,66 @@
 import { Component } from "react"
+import Link from "next/link"
+import Router from "next/router"
+
+import Notification from "../../../components/notifications"
+import { pageWrapper } from "../../../utils/wrapper"
+import { sendGet } from "../../../utils/request";
 
 class ManagePostPage extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            title: "",
+            content: "",
+            posts: []
+        }
+    }
+    componentDidMount() {
+        sendGet("/api/posts")
+        .then(res => {
+            if (res.status === 200 && res.data.success === true) {
+                this.setState({ posts: res.data.docs})
+            }
+        })
+        .catch(err => {
+            console.log("Error in componentDidMount ManagePostPage")
+            console.log(err)
+        })
+    }
+
+    renderPosts() {
+        const posts = this.state.posts
+        if (!Array.isArray(posts) || posts.length === 0) {
+            return (<h2>HAVEN'T ANY POSTS</h2>)
+        }
+        return posts.map((elem, index) => {
+            return (
+                <li key={index} style={{ display: "flex", justifyContent: "space-between" }}>
+                    <span>{elem.title}</span>
+                    <span><a href="#">Edit</a></span>
+                </li>
+            )
+        })
+    }
+
     render() {
         return (
-            <div>Post-manage</div>
+            <div style={{ maxWidth: 700, margin: "30px auto" }}>
+                <Link href="/manages">
+                <a><h3>Back</h3></a>
+                </Link>
+        
+                <Link href="/manages/post-management/new">
+                <a><h3>Create new post</h3></a>
+                </Link>
+    
+                <ul>
+                {this.renderPosts()}
+                </ul>
+          </div>
         )
     }
 }
 
-export default ManagePostPage;
+export default pageWrapper(null)(ManagePostPage)
--- pages/manages/post-management/new.js
+++ pages/manages/post-management/new.js
@@ -1,11 +1,71 @@
 import { Component } from "react"
+import Link from "next/link"
+import { Button, Input, Tag } from "antd"
+
+import Notification from "../../../components/notifications"
+import { pageWrapper } from "../../../utils/wrapper"
+import { sendPost } from "../../../utils/request";
+
+const { TextArea } = Input;
+const CheckableTag = Tag.CheckableTag;
+
+const tagsFromServer = ['Movies', 'Books', 'Music', 'Sports'];
 
 class ManageNewPostPage extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            title: "",
+            content: "",
+            selectedTags: []
+        }
+    }
+    handleChange(tag, checked) {
+        const { selectedTags } = this.state;
+        const nextSelectedTags = checked
+            ? [...selectedTags, tag]
+            : selectedTags.filter(t => t !== tag);
+        console.log('You are interested in: ', nextSelectedTags);
+        this.setState({ selectedTags: nextSelectedTags });
+    }
+
+    handleCreateNewPost = () => {
+        sendPost("/api/posts", null, this.state)
+        .then(res => console.log(res))
+        .then(err => console.log(err))
+    }
+
     render() {
+        const { selectedTags } = this.state;
         return (
-            <div>Post-newmanage</div>
+            <div style={{ maxWidth: 700, margin: "30px auto" }} className="new-form-posts">
+            <Link href="/manage/post-management">
+              <a><h3>Back</h3></a>
+            </Link>
+            <h3>Title</h3>
+            <Input onChange={e => this.setState({ title: e.target.value })} style={{ width: "100%", height: 32 }} />
+    
+            <h3>Content</h3>
+            <TextArea
+              onChange={e => this.setState({ content: e.target.value })}
+              style={{ backgroundColor: "#d1d1d1", borderColor: "#d1d1d1", fontSize: 14, width: "100%", minHeight: 300 }}
+            /><br />
+                  <div>
+                    <h6 style={{ marginRight: 8, display: 'inline' }}>Tag:</h6>
+                    {tagsFromServer.map(tag => (
+                    <CheckableTag
+                        key={tag}
+                        checked={selectedTags.indexOf(tag) > -1}
+                        onChange={checked => this.handleChange(tag, checked)}
+                    >
+                        {tag}
+                    </CheckableTag>
+                    ))}
+                </div>
+            <Button type="danger" onClick={() => this.handleCreateNewPost()}>Submit</Button>
+          </div>
         )
     }
 }
 
-export default ManageNewPostPage;
+export default pageWrapper(null)(ManageNewPostPage);
